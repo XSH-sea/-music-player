5f9ab2fda28cbd05ce41ce80aba6fe523b80cea5
XSH-sea <44542714+XSH-sea@users.noreply.github.com>
2019-08-07 13:40:18 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,26 +5,26 @@ $(function () {
             songPic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004SVr621zymKi.jpg?max_age=2592000',
             name: '大风吹',
             singer: '曾沛慈',
-            src: 'http://mobileoc.music.tc.qq.com/C400001jkbBE3XT6Ez.m4a?vkey=9C88BD4BE94C61052E55D75AF810BEEFEC89CD2C2C922D4A32764ECD0EC418739FC5329332251B45562D9346FAC03AA2C8259E71C9156E99&guid=jlwz.cn&uin=0&fromtag=8'
+            src: 'http://music.163.com/song/media/outer/url?id=1378093025.mp3'
         },
         {
             songPic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004NkJKq0GkHOa.jpg?max_age=2592000',
             name: '路过人间',
             singer: '郁可唯',
-            src: 'http://mobileoc.music.tc.qq.com/C4000042jyJt0aQ0Gd.m4a?vkey=6E9CFE08D431358D5BD9F79FF66849192AF0817BF0EBD0F3841A39E97525B37AB4FA2453444291E74E1144B682DBC4F1C6FB185B37CE13C7&guid=jlwz.cn&uin=0&fromtag=8'
+            src: 'http://music.163.com/song/media/outer/url?id=1354477202.mp3'
 
         },
         {
             songPic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000MPe4T3iM5bb.jpg?max_age=2592000',
             name: '沙文',
             singer: '林忆莲',
-            src: 'http://mobileoc.music.tc.qq.com/C400003ppgkd1h2oGU.m4a?vkey=3430986B0840AE2BC9EDE894B81D906FCCCA36C60EF5DC940A3E2B8D2501A644B311E21E31144CC21C7E3BFB9D022BDDE0BE6F2CDDB71DD6&guid=jlwz.cn&uin=0&fromtag=8'
+            src: 'http://music.163.com/song/media/outer/url?id=1334667453.mp3'
         },
         {
             songPic: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001dQbq01b2bra.jpg?max_age=2592000',
             name: '你瞒我瞒',
             singer: '陈柏宇',
-            src: 'http://mobileoc.music.tc.qq.com/C400000XJvlv11ltfF.m4a?vkey=C1571BCE54FB6E0283B0637A9432F06ED63AD4C18FF54B17305483AD1F381CA11FBA223F6EBDAE2FA1459AC27A24D45AA4C184664D9513B3&guid=jlwz.cn&uin=0&fromtag=8'
+            src: 'http://music.163.com/song/media/outer/url?id=25718007.mp3'
         },
     ]
 
